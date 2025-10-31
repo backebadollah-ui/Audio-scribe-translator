95e29b7860474a4cb4cff068cd8435449de894e3
backebadollah-ui <back.ebadollah@gmail.com>
2025-10-31 15:06:50 +0330

    Add Config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       base: "/Audio-scribe-translator/",
+      build: {
+      outDir: 'docs',
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
